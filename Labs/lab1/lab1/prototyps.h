bc2e630d8bde10f22cfda0b2853068b3139383b2
fallen-Ikaroc <goshakkoshak65@gmail.com>
2020-10-30 10:54:34 +0300

    111

--- Labs/lab1/lab1/prototyps.h
+++ Labs/lab1/lab1/prototyps.h
@@ -3,23 +3,21 @@
 void maininterface();
 
 void seminar1();
-void seminar2();
-void seminar3();
-
 double* ArrayIn(int size);
-void ArrayOut(int size, double* array);
-void ArrayOut(int size, int sizec, int** array);
-
-int** ArrayIn(int size, int sizec);
 int* Minimal(double* array, int size, int &minimal_quantity);
 double sum(int size, double* array, int& k, bool& error);
-int sum(int size, int sizec, int** array);
 double* sort(int size, double* array);
+void ArrayOut(int size, double* array);
 
+void seminar2();
+int** ArrayIn(int size, int sizec);
+int sum(int size, int sizec, int** array);
 int** changec(int size, int** array, int k, int j);
 int character(int size, int sizec, int** array, int j);
 int** sort(int size, int sizec, int** array, bool& k);
+void ArrayOut(int size, int sizec, int** array);
 
+void seminar3();
 char** in(bool &a);
 void out(char **text);
 char* search(char text[1000], bool &q, int &c);
