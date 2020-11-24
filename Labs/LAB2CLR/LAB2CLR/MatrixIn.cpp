0fa3ab0b857c59377b0e6d41faf2e6ba30920f2d
Mistral59 <68201877+Mistral59@users.noreply.github.com>
2020-11-24 09:41:31 +0300

    Update 3.0

--- Labs/LAB2CLR/LAB2CLR/MatrixIn.cpp
+++ Labs/LAB2CLR/LAB2CLR/MatrixIn.cpp
@@ -9,9 +9,7 @@ array<int,2>^ MatrixIn(int n, int m)
 	array<int, 2>^ matrix = gcnew array<int, 2>(n, m);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
-		{
 			matrix[i, j] = rnd->Next() / 10000000 - 100;
-		}
 	return matrix;
 }
 
--- Labs/LAB2CLR/LAB2CLR/main.cpp
+++ Labs/LAB2CLR/LAB2CLR/main.cpp
@@ -4,11 +4,9 @@ using namespace System;
 
 int main()
 {
-
     int n = 4, m = 7;
-    char a;
-    int _a;
-    bool checkForCycle = true;
+    char InputAction;
+    bool checkForCycle = true, CheckForNegative, CheckForP = false, CheckForQ = false;
     array<int, 2>^ P, ^ Q;
     array<int>^ R, ^ T;
     Console::Write("Lab 2\nSpecify values for integer elements of matrixes P and Q with dimension 4 by 7\nand form arrays R and T from the sums of negative elements of matrix rows P and\nQ respectively.\n");
@@ -22,18 +20,82 @@ int main()
         Console::WriteLine("[6] - Calculate array T and out.");
         Console::WriteLine("[0] - To finish.");
         Console::Write("Your choice: ");
-        a = Console::Read();
+        InputAction = Console::Read();
         Console::ReadLine();
-        _a = a - 48;
-        switch (_a) {
-        case 1: P = MatrixIn(n, m); Console::WriteLine("\nMatrix P generate!\n"); break;
-        case 2: Q = MatrixIn(n, m); Console::WriteLine("\nMatrix Q generate!\n"); break;
-        case 3: Console::Write("\nMatrix P:");  MatrixOut(n, m, P); break;
-        case 4: Console::Write("\nMatrix Q:");  MatrixOut(n, m, Q); break;
-        case 5: R = Sum(n, m, P); Console::Write("\nMatrix R:"); MatrixOut(R); break;
-        case 6: T = Sum(n, m, Q); Console::Write("\nMatrix T:"); MatrixOut(T); break;
-        case 0: checkForCycle = false; break;
-        default: Console::WriteLine("Wrong action!"); break;
+        switch (InputAction - 48)
+        {
+        case 1:
+            P = MatrixIn(n, m);
+            CheckForP = true;
+            Console::WriteLine("\nMatrix P generate!\n");
+            break;
+        case 2:
+            Q = MatrixIn(n, m);
+            CheckForQ = true;
+            Console::WriteLine("\nMatrix Q generate!\n");
+            break;
+        case 3:
+            if (CheckForP)
+            {
+                Console::Write("\nMatrix P:");
+                MatrixOut(n, m, P);
+            }
+            else
+                Console::WriteLine("\nError! Enter the matrix P.\n");
+            break;
+        case 4:
+            if (CheckForQ)
+            {
+                Console::Write("\nMatrix Q:");
+                MatrixOut(n, m, Q);
+            }
+            else
+                Console::WriteLine("\nError! Enter the matrix Q.\n");
+            break;
+        case 5:
+            if (CheckForP)
+            {
+                R = Sum(n, m, P);
+                CheckForNegative = false;
+                for (int i = 0; i < n; i++)
+                    if (R[i] == 0)
+                        CheckForNegative = true;
+                if (CheckForNegative)
+                    Console::WriteLine("Error! There is no one negative elements in the one of the rows.");
+                else
+                {
+                    Console::Write("\nMatrix R:");
+                    MatrixOut(R);
+                }
+            }
+            else
+                Console::WriteLine("\nError! Enter the matrix P.\n");
+            break;
+        case 6:
+            if (CheckForQ)
+            {
+                T = Sum(n, m, Q);
+                CheckForNegative = false;
+                for (int i = 0; i < n; i++)
+                    if (T[i] == 0)
+                        CheckForNegative = true;
+                if (CheckForNegative)
+                    Console::WriteLine("Error! There is no one negative elements in the one of the rows.");
+                else
+                {
+                    Console::Write("\nMatrix T:");
+                    MatrixOut(T);
+                }
+            }
+            else
+                Console::WriteLine("\nError! Enter the matrix Q.\n");
+            break;
+        case 0:
+            checkForCycle = false;
+            break;
+        default:
+            Console::WriteLine("Wrong action!");
+            break;
         }
     }
     return 0;
