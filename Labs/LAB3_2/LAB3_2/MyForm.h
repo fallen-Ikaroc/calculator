6574735a7fcb400af608c4de546ec7b9b44e262f
Mistral59 <68201877+Mistral59@users.noreply.github.com>
2020-11-29 15:12:51 +0300

    Update_Lab3_2_final

--- Labs/LAB3_2/LAB3_2/MyForm.h
+++ Labs/LAB3_2/LAB3_2/MyForm.h
@@ -1,7 +1,9 @@
 #pragma once
+#define _USE_MATH_DEFINES
 #include <cmath>
-namespace LAB32 {
-
+#define M_PI           3.14159265358979323846 
+namespace LAB32
+{
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -36,9 +38,6 @@ namespace LAB32 {
 			}
 		}
 	private: System::Windows::Forms::TextBox^ line;
-	protected:
-
-
 	private: System::Windows::Forms::Button^ start;
 	private: System::Windows::Forms::Label^ Lheight;
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
@@ -70,9 +69,11 @@ namespace LAB32 {
 			// 
 			// line
 			// 
-			this->line->Location = System::Drawing::Point(3, 22);
+			this->line->Font = (gcnew System::Drawing::Font(L"Times New Roman", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->line->Location = System::Drawing::Point(3, 27);
 			this->line->Name = L"line";
-			this->line->Size = System::Drawing::Size(100, 20);
+			this->line->Size = System::Drawing::Size(100, 32);
 			this->line->TabIndex = 0;
 			this->line->TextChanged += gcnew System::EventHandler(this, &MyForm::line_TextChanged);
 			this->line->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &MyForm::line_KeyPress);
@@ -80,22 +81,26 @@ namespace LAB32 {
 			// start
 			// 
 			this->start->Enabled = false;
-			this->start->Location = System::Drawing::Point(3, 45);
+			this->start->Font = (gcnew System::Drawing::Font(L"Times New Roman", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->start->Location = System::Drawing::Point(3, 65);
 			this->start->Name = L"start";
-			this->start->Size = System::Drawing::Size(75, 23);
+			this->start->Size = System::Drawing::Size(80, 28);
 			this->start->TabIndex = 2;
-			this->start->Text = L"start";
+			this->start->Text = L"Start";
 			this->start->UseVisualStyleBackColor = true;
 			this->start->Click += gcnew System::EventHandler(this, &MyForm::start_Click);
 			// 
 			// Lheight
 			// 
 			this->Lheight->AutoSize = true;
+			this->Lheight->Font = (gcnew System::Drawing::Font(L"Times New Roman", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->Lheight->Location = System::Drawing::Point(3, 0);
 			this->Lheight->Name = L"Lheight";
-			this->Lheight->Size = System::Drawing::Size(23, 13);
+			this->Lheight->Size = System::Drawing::Size(139, 23);
 			this->Lheight->TabIndex = 4;
-			this->Lheight->Text = L"line";
+			this->Lheight->Text = L"Enter parametr:";
 			this->Lheight->UseWaitCursor = true;
 			// 
 			// tableLayoutPanel1
@@ -119,16 +124,16 @@ namespace LAB32 {
 			this->tableLayoutPanel2->ColumnCount = 1;
 			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				50)));
-			this->tableLayoutPanel2->Controls->Add(this->line, 0, 1);
 			this->tableLayoutPanel2->Controls->Add(this->Lheight, 0, 0);
+			this->tableLayoutPanel2->Controls->Add(this->line, 0, 1);
 			this->tableLayoutPanel2->Controls->Add(this->start, 0, 2);
 			this->tableLayoutPanel2->Dock = System::Windows::Forms::DockStyle::Top;
 			this->tableLayoutPanel2->Location = System::Drawing::Point(310, 3);
 			this->tableLayoutPanel2->Name = L"tableLayoutPanel2";
 			this->tableLayoutPanel2->RowCount = 3;
-			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 46.15385F)));
-			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 53.84615F)));
-			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 299)));
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 38.88889F)));
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 61.11111F)));
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 279)));
 			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 21)));
 			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 238)));
 			this->tableLayoutPanel2->Size = System::Drawing::Size(181, 342);
@@ -150,7 +155,7 @@ namespace LAB32 {
 			this->ClientSize = System::Drawing::Size(493, 350);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Task 2";
 			this->tableLayoutPanel1->ResumeLayout(false);
 			this->tableLayoutPanel2->ResumeLayout(false);
 			this->tableLayoutPanel2->PerformLayout();
@@ -158,10 +163,9 @@ namespace LAB32 {
 			this->ResumeLayout(false);
 
 		}
+
 #pragma endregion
-	private: delegate double Function(double);
-	private: double f(double x) { return Math::Sin(x); }
-	private: double x1 = -2 * Math::PI, x2 = 2 * Math::PI, xstep, y1, y2, ystep;
+
 	private: System::Void start_Click(System::Object^ sender, System::EventArgs^ e)
 	{
 		Regex^ r = gcnew Regex("^[0-9]*[,]?[0-9]+$");
@@ -169,18 +173,18 @@ namespace LAB32 {
 		if (value->Success)
 		{
 			int pw = pictureBox1->Width, ph = pictureBox1->Height;
-			Function^ f = gcnew Function(this, &MyForm::f);
 			Bitmap^ img = gcnew Bitmap(pw, ph);
 			Graphics^ g = Graphics::FromImage(img);
 			int mx = pw / 2, my = ph / 2;
-			const double pi = 3.14159265358979323846;
 			double a = Convert::ToDouble(line->Text);
 			array<double>^ x = gcnew array<double>(6), ^ y = gcnew array<double>(6);
-			for (double ys = 0; ys < ph; ys += a) {
-				for (double xs = 0; xs < pw; xs += a) {
+			for (double ys = 0; ys < ph; ys += a)
+			{
+				for (double xs = 0; xs < pw; xs += a) 
+				{
 					for (int i = 0; i < 6; i++)
 					{
-						double angle = pi * i / 3;
+						double angle = M_PI * i / 3;
 						x[i] = cos(angle) * a  + xs;
 						y[i] = sin(angle) * a  + ys;
 					}
@@ -188,23 +192,22 @@ namespace LAB32 {
 					SolidBrush^ shadowBrush = gcnew SolidBrush(customColor);
 					g->FillPolygon(shadowBrush, gcnew array<Point>{
 						Point(x[0], y[0]),
-							Point(x[1], y[1]),
-							Point(x[2], y[2]),
-							Point(x[3], y[3]),
-							Point(x[4], y[4]),
-							Point(x[5], y[5])
+						Point(x[1], y[1]),
+						Point(x[2], y[2]),
+						Point(x[3], y[3]),
+						Point(x[4], y[4]),
+						Point(x[5], y[5])
 					});
 					g->DrawPolygon(Pens::Red, gcnew array<Point>{
 						Point(x[0], y[0]),
-							Point(x[1], y[1]),
-							Point(x[2], y[2]),
-							Point(x[3], y[3]),
-							Point(x[4], y[4]),
-							Point(x[5], y[5])
+						Point(x[1], y[1]),
+						Point(x[2], y[2]),
+						Point(x[3], y[3]),
+						Point(x[4], y[4]),
+						Point(x[5], y[5])
 					});
 				}
 			}
-
 			this->pictureBox1->Image = img;
 		}
 		else
