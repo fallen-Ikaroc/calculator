03e94827f5d0e601eefaf4dbcb67dff7f323b043
fallen-Ikaroc <goshakkoshak65@gmail.com>
2020-12-18 16:23:00 +0300

    db1

--- Labs/Kurs/Kurs/Form1.Designer.cs
+++ Labs/Kurs/Kurs/Form1.Designer.cs
@@ -41,7 +41,6 @@ namespace Kurs
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -49,21 +48,22 @@ namespace Kurs
             this.proceduresDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.medicinesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.quantityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.medicinesBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.medicinesTableAdapter = new Kurs.Database1DataSetTableAdapters.medicinesTableAdapter();
             this.idDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.quantityDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.analogDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.medicinesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.medicinesTableAdapter = new Kurs.Database1DataSetTableAdapters.medicinesTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.illnessBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
-            this.tabPage3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.medicinesBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -95,7 +95,7 @@ namespace Kurs
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1214, 523);
+            this.tabControl1.Size = new System.Drawing.Size(1257, 575);
             this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
             this.tabControl1.TabIndex = 0;
             // 
@@ -108,7 +108,7 @@ namespace Kurs
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1206, 497);
+            this.tabPage1.Size = new System.Drawing.Size(1249, 549);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Main Page";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -116,7 +116,7 @@ namespace Kurs
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 90);
+            this.label1.Location = new System.Drawing.Point(35, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 6;
@@ -124,22 +124,23 @@ namespace Kurs
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 87);
+            this.textBox1.Location = new System.Drawing.Point(140, 43);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(129, 20);
             this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(683, 237);
+            this.comboBox1.Location = new System.Drawing.Point(1079, 6);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(367, 246);
+            this.button1.Location = new System.Drawing.Point(297, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -151,21 +152,10 @@ namespace Kurs
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1206, 497);
+            this.tabPage2.Size = new System.Drawing.Size(1249, 549);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Illnesses Table ";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.dataGridView2);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1206, 497);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Medicines Table";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -220,6 +210,17 @@ namespace Kurs
             this.quantityDataGridViewTextBoxColumn.HeaderText = "quantity";
             this.quantityDataGridViewTextBoxColumn.Name = "quantityDataGridViewTextBoxColumn";
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1249, 549);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Medicines Table";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // dataGridView2
             // 
             this.dataGridView2.AutoGenerateColumns = false;
@@ -234,15 +235,6 @@ namespace Kurs
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(445, 150);
             this.dataGridView2.TabIndex = 0;
-            // 
-            // medicinesBindingSource
-            // 
-            this.medicinesBindingSource.DataMember = "medicines";
-            this.medicinesBindingSource.DataSource = this.database1DataSetBindingSource;
-            // 
-            // medicinesTableAdapter
-            // 
-            this.medicinesTableAdapter.ClearBeforeFill = true;
             // 
             // idDataGridViewTextBoxColumn1
             // 
@@ -268,6 +260,15 @@ namespace Kurs
             this.analogDataGridViewTextBoxColumn.HeaderText = "analog";
             this.analogDataGridViewTextBoxColumn.Name = "analogDataGridViewTextBoxColumn";
             // 
+            // medicinesBindingSource
+            // 
+            this.medicinesBindingSource.DataMember = "medicines";
+            this.medicinesBindingSource.DataSource = this.database1DataSetBindingSource;
+            // 
+            // medicinesTableAdapter
+            // 
+            this.medicinesTableAdapter.ClearBeforeFill = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -276,7 +277,6 @@ namespace Kurs
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.illnessBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
@@ -284,8 +284,8 @@ namespace Kurs
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
-            this.tabPage3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.medicinesBindingSource)).EndInit();
             this.ResumeLayout(false);
